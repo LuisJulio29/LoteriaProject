4d68fab1158181dc86d1c0bdfa322dfc9a055bf9
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2025-01-28 12:37:31 -0500

    datetime date

--- LoteriaProject/Model/Ticket.cs
+++ LoteriaProject/Model/Ticket.cs
@@ -1,10 +1,13 @@
-﻿namespace LoteriaProject.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoteriaProject.Model
 {
     public class Ticket
     {
         public int Id { get; set; }
+        [ MaxLength(4), MinLength(3)]
         public required string Number { get; set; }
-        public required string Date { get; set; }
+        public required DateTime Date { get; set; }
         public required string Loteria { get; set; }
         public required string Jornada { get; set; }
 
