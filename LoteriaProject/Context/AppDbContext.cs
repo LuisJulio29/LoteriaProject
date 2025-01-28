e03c2b4b1362f73999bc67106497ec469d66470f
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2025-01-28 08:53:44 -0500

    Agregar archivos de proyecto.

---
+++ LoteriaProject/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using LoteriaProject.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoteriaProject.Context
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+}
---
+++ LoteriaProject/Model/Ticket.cs
@@ -0,0 +1,12 @@
+﻿namespace LoteriaProject.Model
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public required string Number { get; set; }
+        public required string Date { get; set; }
+        public required string Loteria { get; set; }
+        public required string Jornada { get; set; }
+
+    }
+}
