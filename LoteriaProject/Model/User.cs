db47115eebe35eb58ca055e07575f50211daea56
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2025-01-28 20:19:34 -0500

    Create User

---
+++ LoteriaProject/Model/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LoteriaProject.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string UserName { get; set; }
+        public required string Password { get; set; }
+        public required UserRole Role { get; set; }
+
+    }
+    public enum UserRole
+    {
+        Admin,
+        User
+    }
+}
