65c62dac32dc7ea917a9fc8bbfe3d5e34f05336d
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2025-01-28 20:21:25 -0500

    Create EncryptPassword and GenerateJwtToken where utilities

---
+++ LoteriaProject/Custom/Utilities.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Security.Cryptography;
+using LoteriaProject.Model;
+
+namespace LoteriaProject.Custom
+{
+    public class Utilities
+    {
+        private readonly IConfiguration _configuration;
+        public Utilities(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string EncryptPassword(string password)
+        {
+            using var sha256 = SHA256.Create();
+            var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+        }
+
+        public string GenerateJwtToken(User user)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.Role, user.Role.ToString())
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(_configuration["Jwt:Issuer"],
+                _configuration["Jwt:Issuer"],
+                claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: creds);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
