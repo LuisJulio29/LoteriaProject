4c935e8ccffe134320ae30a42a008b8f9897c7dc
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2025-02-04 11:06:34 -0500

    Add sing Migration db

---
+++ LoteriaProject/Migrations/20250204144203_Add field sing in ticket.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoteriaProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addfieldsinginticket : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "sing",
+                table: "Tickets",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "sing",
+                table: "Tickets");
+        }
+    }
+}
