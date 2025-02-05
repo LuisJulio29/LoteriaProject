d3b1a9f15ecd7e6c109df2e90058f44a077559d0
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2025-02-04 20:12:31 -0500

    Add AstroPatronsModel

---
+++ LoteriaProject/Model/AstroPatron.cs
@@ -0,0 +1,30 @@
+﻿namespace LoteriaProject.Model
+{
+    public class AstroPatron
+    {
+        public int Id { get; set; }
+        public required DateTime Date { get; set; }
+        public required string Jornada { get; set; }
+        public required AstroSign[] Sign { get; set; }
+        public required int[] Row1 { get; set; }
+        public required int[] Row2 { get; set; }
+        public required int[] Row3 { get; set; }
+        public required int[] Row4 { get; set; }
+
+    }
+    public enum AstroSign
+    {
+        Aries = 1,
+        Tauro = 2,
+        Geminis = 3,
+        Cancer = 4,
+        Leo = 5,
+        Virgo = 6,
+        Libra = 7,
+        Escorpio = 8,
+        Sagitario = 9,
+        Capricornio = 10,
+        Acuario = 11,
+        Piscis = 12
+    }
+}
