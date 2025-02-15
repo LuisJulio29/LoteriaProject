1f1468fad1836184edc800c2eb6928da29d23f65
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2025-02-15 12:16:03 -0500

    add Number Length min (3) for Pick 3

--- LoteriaProject/Model/Ticket.cs
+++ LoteriaProject/Model/Ticket.cs
@@ -5,7 +5,7 @@ namespace LoteriaProject.Model
     public class Ticket
     {
         public int Id { get; set; }
-        [ MaxLength(4), MinLength(4)]
+        [ MaxLength(4), MinLength(3)]
         public required string Number { get; set; }
         public string? sign { get; set; }
         public required DateTime Date { get; set; }
