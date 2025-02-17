cf0c658269c2ed48c9a2475851724ac2e4146c2b
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2025-02-17 13:44:03 -0500

    new model of Loteria

---
+++ LoteriaProject/Model/Sorteo.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoteriaProject.Model
+{
+    public class Sorteo
+    {
+        public int Id { get; set; }
+        [MaxLength(4), MinLength(4)]
+        public required string Number { get; set; }
+        [MaxLength(3), MinLength(3)]
+        public required string Serie { get; set; }
+        public required DateTime Date { get; set; }
+        public required string Loteria { get; set; }
+        public required string Jornada { get; set; }
+    }
+}
