1f849df767b5cc17deba74a036a3efc921f028f4
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2025-02-18 16:03:58 -0500

    fix sorteo model

---
+++ LoteriaProject/Migrations/20250218210312_delete jornada for Sorteo.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoteriaProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class deletejornadaforSorteo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Jornada",
+                table: "Sorteos");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Jornada",
+                table: "Sorteos",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
