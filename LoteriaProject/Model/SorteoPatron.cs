06c85d3b64b04f400dbacbd17e541d550f2c02dc
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2025-02-20 09:43:59 -0500

    new SorteoPatron model

---
+++ LoteriaProject/Model/SorteoPatron.cs
@@ -0,0 +1,9 @@
+﻿namespace LoteriaProject.Model
+{
+    public class SorteoPatron
+    {
+        public int Id { get; set; }
+        public required DateTime Date { get; set; }
+        public required int[] PatronNumbers { get; set; }
+    }
+}
